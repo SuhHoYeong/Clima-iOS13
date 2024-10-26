bf814dfa7f80d38ec22fcc635e51deca2bbf2dab
SuhHoYeong <hyoung0976@gmail.com>
2024-10-26 13:19:03 +0900

    weatehrComposition

---
+++ Clima/Model/WeatehrData.swift
@@ -0,0 +1,24 @@
+//
+//  weatehrData.swift
+//  Clima
+//
+//  Created by 서호영 on 10/22/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable{
+    let description: String
+    let id: Int
+}
